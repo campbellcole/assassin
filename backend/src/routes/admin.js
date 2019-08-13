0503cdd1ae9158a2191e6ea928ef975587542e01
Campbell Cole <cmcoleco@gmail.com>
2019-08-13 11:52:50 -0700

    remove comment

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -54,46 +54,5 @@ router.get('/:command/:username', (req, res) => {
   if (RET.UNKNOWN === dbres) return res.status(406).json({ errors: 'unknown command' });
   else return res.status(200).end();
 });
-/*
-router.get('/verify/:username', (req, res) => {
-  if (LVL.ADMIN !== authLevel(req)) return res.status(401).send();
-  var { username } = req.params;
-  var dbres = db.verifyUser(username);
-  if (RET.OK !== dbres) return res.status(406).json({ errors: dbres });
-  else return res.status(200).end();
-});
 
-router.get('/deverify/:username', (req, res) => {
-  if (LVL.ADMIN !== authLevel(req)) return res.status(401).send();
-  var { username } = req.params;
-  var dbres = db.deverifyUser(username);
-  if (RET.OK !== dbres) return res.status(406).json({ errors: dbres });
-  else return res.status(200).end();
-});
-
-router.get('/promote/:username', (req, res) => {
-  if (LVL.ADMIN !== authLevel(req)) return res.status(401).send)();
-  var { username } = req.params;
-  var dbres = db.promoteUser(username);
-  if (RET.OK != dbres) return res.status(406).json({ errors: dbres });
-  else return res.status(200).end();
-});
-
-router.get('/demote/:username', (req, res) => {
-  if (LVL.ADMIN !== authLevel(req)) return res.status(401).send();
-  var { username } =
-})
-
-router.get('/user/:username', (req, res) => {
-  var { username } = req.params;
-  var al = authLevel(req);
-  if (LVL.USER === al) {
-    if (username !== req.user.username) return res.status(401).send();
-  }
-  if (LVL.NONE === al) return res.status(401).send();
-  var dbres = db.getUser(username);
-  if (RET.USER_NOT_FOUND === dbres) return res.status(406).json({ errors: dbres });
-  else return res.send(dbres);
-});
-*/
 export default router;
